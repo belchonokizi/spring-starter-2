1eedfeb53856b7b505d5374182dba006f7ed5131
belchonokizi <belchonokizi@yandex.ru>
2024-07-06 16:55:59 +0300

    add @Component and @Autowired

--- src/main/java/com/dmdev/spring/bfpp/LogBeanFactoryPostProcessor.java
+++ src/main/java/com/dmdev/spring/bfpp/LogBeanFactoryPostProcessor.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.ConstructorArgumentValues;
 import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, Ordered {
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
--- src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
@@ -1,18 +1,26 @@
 package com.dmdev.spring.database.repository;
 
-import com.dmdev.spring.bpp.InjectBean;
 import com.dmdev.spring.bpp.Transaction;
 import com.dmdev.spring.database.entity.Company;
 import com.dmdev.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import java.util.Optional;
 
+@Repository
 @Transaction
 public class CompanyRepository implements CrudRepository<Integer, Company> {
 
-    @InjectBean
-    private ConnectionPool connectionPool;
+    private final ConnectionPool pool1;
+    private final Integer poolSize;
+
+    public CompanyRepository(ConnectionPool pool1,
+                             @Value("${db.pool.size}") Integer poolSize) {
+        this.pool1 = pool1;
+        this.poolSize = poolSize;
+    }
 
     @PostConstruct
     private void init() {
