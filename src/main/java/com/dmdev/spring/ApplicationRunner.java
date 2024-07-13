044dd10af9ed2df38072aecd5046a4edd9d6221d
belchonokizi <belchonokizi@yandex.ru>
2024-07-13 15:26:31 +0300

    add spring boot

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -1,29 +1,13 @@
 package com.dmdev.spring;
 
-import com.dmdev.spring.config.ApplicationConfiguration;
-import com.dmdev.spring.database.pool.ConnectionPool;
-import com.dmdev.spring.database.repository.CrudRepository;
-import com.dmdev.spring.service.CompanyService;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.Serializable;
-
+@SpringBootApplication
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        String value = "hello";
-        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
-        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
-        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
-
-        try (var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
-            //      clazz -> String -> Map<String, Object>
-            var connectionPool = context.getBean("connectionPool", ConnectionPool.class);
-            System.out.println(connectionPool);
-
-            var companyService = context.getBean("companyService", CompanyService.class);
-            System.out.println(companyService.findById(1));
-        }
+        SpringApplication.run(ApplicationRunner.class, args);
     }
 }
+
--- src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
@@ -1,11 +1,11 @@
 package com.dmdev.spring.database.pool;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 @Component
 public class ConnectionPool {
