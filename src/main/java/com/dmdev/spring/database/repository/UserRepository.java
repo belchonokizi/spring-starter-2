9b904c84cd93ac2e815aa278e1f792d8a99afb42
belchonokizi <belchonokizi@yandex.ru>
2024-07-29 14:45:55 +0300

    use @Modifying

--- src/main/java/com/dmdev/spring/database/repository/UserRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.dmdev.spring.database.repository;
 
+import com.dmdev.spring.database.entity.Role;
 import com.dmdev.spring.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -9,7 +11,7 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     //% -используются для containing
-    //SimpleJpaQuery
+    //SimpleJpaQuery - более предпочтительный способ
     @Query("select u from User u " +
            "where u.firstname like %:firstname% and u.lastname like %:lastname%")
     List<User> findAllBy(String firstname, String lastname);
@@ -18,4 +20,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT * FROM users u WHERE u.username = :username",
             nativeQuery = true)
     List<User> findAllByUsername(String username);
+
+    //автоматическая очистка PersistenceContext до выполнения
+    @Modifying(clearAutomatically = true)
+    @Query("update User u " +
+           "set u.role = :role " +
+           "where u.id in (:ids)")
+    int updateRole(Role role, Long... ids);
 }
--- src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryIT.java
+++ src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryIT.java
@@ -1,5 +1,6 @@
 package com.dmdev.spring.integration.database.repository;
 
+import com.dmdev.spring.database.entity.Role;
 import com.dmdev.spring.database.entity.User;
 import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.spring.integration.annotation.IT;
@@ -8,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @IT
 @RequiredArgsConstructor
@@ -20,5 +21,23 @@ class UserRepositoryIT {
     void checkQueries() {
         List<User> users = userRepository.findAllBy("a", "ov");
         assertEquals(2, users.size());
+    }
+
+    @Test
+    void checkUpdate() {
+        User user = userRepository.getReferenceById(2L);
+        assertEquals(Role.ADMIN, user.getRole());
+
+        int resultCount = userRepository.updateRole(Role.USER, 2L, 5L);
+        assertEquals(2, resultCount);
+
+        /** при следующей проверке тест упадет, так как в PersistenceContext
+         * - first level cache - сохранится старая неизмененная версия
+         * нужно чистить PersistenceContext
+         * после чистки старыми сущностями (user) пользоваться нельзя или
+         * нужно заново положить в PersistenceContext
+         **/
+        User userSame = userRepository.getReferenceById(2L);
+        assertEquals(Role.USER, userSame.getRole());
     }
 }
