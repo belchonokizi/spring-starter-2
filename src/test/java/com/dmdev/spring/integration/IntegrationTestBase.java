c151a742ba88cd1e419b238fd24c6d16144af618
belchonokizi <belchonokizi@yandex.ru>
2024-08-06 13:49:51 +0300

    use testcontainers

---
+++ src/test/java/com/dmdev/spring/integration/IntegrationTestBase.java
@@ -0,0 +1,30 @@
+package com.dmdev.spring.integration;
+
+import com.dmdev.spring.integration.annotation.IT;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+//докер контейнер поднимается 1 для всех тестов
+@IT
+@Sql({
+        "classpath:sql/data.sql"
+})
+public abstract class IntegrationTestBase {
+    //указываем тег докер имеджа
+    private static final PostgreSQLContainer<?> container =
+            new PostgreSQLContainer<>("postgres:14.1");
+
+    @BeforeAll
+    static void runContainer() {
+        container.start();
+    }
+
+    //установка динамического урла
+    @DynamicPropertySource
+    static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+    }
+}
