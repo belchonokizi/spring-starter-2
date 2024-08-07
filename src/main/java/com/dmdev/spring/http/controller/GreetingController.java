3d718d50fe2f450abda1bd8f2a8c2a0c5c9386e0
belchonokizi <belchonokizi@yandex.ru>
2024-08-07 12:16:39 +0300

    use request and session attributes

--- src/main/java/com/dmdev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dmdev/spring/http/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.dmdev.spring.http.controller;
 
+import com.dmdev.spring.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,10 +9,29 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/api/v1")
+//чтобы установить аттрибуты на уровне сессии
+@SessionAttributes({"user"})
 public class GreetingController {
 
-    @GetMapping("/hello/{id}")
+    @GetMapping("/hello")
     public ModelAndView hello(ModelAndView modelAndView,
+                              HttpServletRequest request) {
+        modelAndView.setViewName("greeting/hello");
+        //атрибут на уровне реквеста, если над классом нет @SessionAttributes({"user"})
+        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
+        return modelAndView;
+    }
+
+    @GetMapping("/bye")
+    public ModelAndView bye(ModelAndView modelAndView,
+                            //получаем сессионный атрибут
+                            @SessionAttribute("user") UserReadDto user) {
+        modelAndView.setViewName("greeting/bye");
+        return modelAndView;
+    }
+
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello2(ModelAndView modelAndView,
                               HttpServletRequest request,
                               //параметр будет обязателен
                               @RequestParam Integer age,
@@ -19,12 +39,6 @@ public class GreetingController {
                               @CookieValue("JSESSIONID") String jsessionId,
                               @PathVariable("id") Integer id) {
         modelAndView.setViewName("greeting/hello");
-        return modelAndView;
-    }
-
-    @GetMapping("/bye")
-    public ModelAndView bye(ModelAndView modelAndView) {
-        modelAndView.setViewName("greeting/bye");
         return modelAndView;
     }
 }
