f1aec7f07d414efdea183c6b3ebf96a921038b99
belchonokizi <belchonokizi@yandex.ru>
2024-08-09 13:31:32 +0300

    add Filter query

--- src/main/java/com/dmdev/spring/config/WebConfiguration.java
+++ src/main/java/com/dmdev/spring/config/WebConfiguration.java
@@ -1,16 +1,14 @@
 package com.dmdev.spring.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.convert.Jsr310Converters;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
 //    3 способ задания формата дат
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(Jsr310Converters.StringToLocalDateConverter.INSTANCE);
-    }
+//    @Override
+//    public void addFormatters(FormatterRegistry registry) {
+//        registry.addConverter(Jsr310Converters.StringToLocalDateConverter.INSTANCE);
+//    }
 }
