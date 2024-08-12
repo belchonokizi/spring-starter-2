1371786515ca1eede40baea81a676873f004c4aa
belchonokizi <belchonokizi@yandex.ru>
2024-08-12 17:08:04 +0300

    add PasswordEncoder

--- src/main/java/com/dmdev/spring/config/SecurityConfiguration.java
+++ src/main/java/com/dmdev/spring/config/SecurityConfiguration.java
@@ -1,9 +1,11 @@
 package com.dmdev.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -14,11 +16,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
-                .httpBasic(Customizer.withDefaults());
-                //переопределяем свою страничку логина
-//                .formLogin(login -> login.loginPage("/login")
-//                        .defaultSuccessUrl("/users")
-//                        //даем разрешение для логина всем
-//                        .permitAll());
+//                .httpBasic(Customizer.withDefaults());
+//                переопределяем свою страничку логина
+                .formLogin(login -> login.loginPage("/login")
+                        .defaultSuccessUrl("/users")
+                        //даем разрешение для логина всем
+                        .permitAll());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
