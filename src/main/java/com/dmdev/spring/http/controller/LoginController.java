6131eaba47c5360d1009a70af5ce0454ddb2fda2
belchonokizi <belchonokizi@yandex.ru>
2024-08-12 16:38:44 +0300

    change form login from default to custom

--- src/main/java/com/dmdev/spring/http/controller/LoginController.java
+++ src/main/java/com/dmdev/spring/http/controller/LoginController.java
@@ -1,10 +1,7 @@
 package com.dmdev.spring.http.controller;
 
-import com.dmdev.spring.dto.LoginDto;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -12,11 +9,6 @@ public class LoginController {
     @GetMapping("/login")
     public String loginPage() {
         return "user/login";
-    }
-
-    @PostMapping("/login")
-    public String login(Model model, LoginDto loginDto) {
-        return "redirect:/login";
     }
 
 }
