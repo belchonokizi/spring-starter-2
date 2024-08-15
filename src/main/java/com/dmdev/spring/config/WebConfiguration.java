0d28deadc4d47d26cc0e05aa117a4b2a58309a9a
belchonokizi <belchonokizi@yandex.ru>
2024-08-15 14:41:11 +0300

    internatiolization Thymeleaf

--- src/main/java/com/dmdev/spring/config/WebConfiguration.java
+++ src/main/java/com/dmdev/spring/config/WebConfiguration.java
@@ -1,7 +1,14 @@
 package com.dmdev.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
@@ -11,4 +18,25 @@ public class WebConfiguration implements WebMvcConfigurer {
 //    public void addFormatters(FormatterRegistry registry) {
 //        registry.addConverter(Jsr310Converters.StringToLocalDateConverter.INSTANCE);
 //    }
+
+    //    добавлем, чтобы каждый запрос обрабатывался нижним интерсептором
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    //    бин для хранения выбранной локали
+    @Bean
+    public LocaleResolver localeResolver() {
+        var localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
 }
